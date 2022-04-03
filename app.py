a4aa7b96be56e6d3a36ee335c5830809ed9b7cc2
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-04-03 21:37:12 +0300

    Fix database configuration

--- app.py
+++ app.py
@@ -7,7 +7,11 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+
+uri = getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = getenv("SECRET_KEY")
 
