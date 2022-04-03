5bc03293e877ba67f37fa48758de237cfb48a19c
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-04-03 21:43:18 +0300

    Update SQLAlchemy version in requirements

--- app.py
+++ app.py
@@ -8,10 +8,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 app = Flask(__name__)
 
-uri = getenv("DATABASE_URL")
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://", 1)
-
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = getenv("SECRET_KEY")
 
