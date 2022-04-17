82d66a1ea31881d93d74e78382378e3d793c70f7
Valtteri Kantanen <valtteri.kantanen@helsinki.fi>
2022-04-17 16:30:38 +0300

    Add 'page not found' error

--- errors.py
+++ errors.py
@@ -3,3 +3,7 @@ from flask import flash, redirect
 def authentication_error():
     flash("Sinulla ei ole oikeutta nähdä sivua", category="error")
     return redirect("/error")
+
+def page_not_found():
+    flash("Sivua ei löytynyt", category="error")
+    return redirect("/error")
